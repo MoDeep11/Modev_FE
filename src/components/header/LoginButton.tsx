e87a5cdd8a608ff219dd2ae15b35ad143ba85dfb
taeyeon <binnimhi@naver.com>
2026-05-30 14:24:12 +0900

    feat:: Login 버튼 퍼블리싱

---
+++ src/components/header/LoginButton.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { Colors } from "../../styles/color";
+import Login from "../../assets/Login.svg";
+
+export default function LoginButton() {
+  return (
+    <Wrapper>
+      <Img src={Login}></Img>
+      <Text>로그인</Text>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background-color: ${Colors.background.base};
+  border: 1px solid ${Colors.background.overlay};
+  color: white;
+  padding: 8px 24px;
+  width: 115px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  gap: 6px;
+`;
+
+const Img = styled.img`
+  width: 16px;
+  height: 16px;
+`;
+
+const Text = styled.p`
+  font-size: 16px;
+`;
