94595ca0af5551e45ecb0905dffaf92cad7957db
taeyeon <binnimhi@naver.com>
2026-06-02 20:29:08 +0900

    feat:: folders 컴포넌트 생성

---
+++ src/components/Build/Folders.tsx
@@ -0,0 +1,66 @@
+import TopFolder from "@/assets/folder/topFolder.svg";
+import Folder from "@/assets/folder/folder.svg";
+import File from "@/assets/folder/file.svg";
+import WriteFile from "@/assets/folder/writeFile.svg";
+import styled from "styled-components";
+import { Colors } from "../../styles/color";
+
+const iconMap = {
+  topFolder: TopFolder,
+  folder: Folder,
+  file: File,
+  writeFile: WriteFile,
+};
+
+interface FoldersProps {
+  text: string;
+  file: "topFolder" | "folder" | "file" | "writeFile";
+  depth: number;
+}
+
+export default function Folders({ text, file, depth }: FoldersProps) {
+  return (
+    <Wrapper depth={depth}>
+      {Array.from({ length: depth }).map((_, index) => (
+        <TreeLine key={index} index={index} />
+      ))}
+      <Icon src={iconMap[file]} alt={text} />
+      <Text file={file}>{text}</Text>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div<{ depth: number }>`
+  position: relative;
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  padding-left: ${(props) => props.depth * 24 + 12}px;
+  height: 26px;
+`;
+
+const TreeLine = styled.div<{ index: number }>`
+  position: absolute;
+  left: ${(props) => props.index * 24 + 20}px;
+  top: 0;
+  width: 1px;
+  height: 100%;
+  background-color: ${Colors.border.strong || Colors.border.default};
+`;
+
+const Icon = styled.img`
+  width: 16px;
+  height: 16px;
+  z-index: 1;
+`;
+
+const Text = styled.p<{ file: string }>`
+  font-size: 14px;
+  color: ${(props) =>
+    props.file === "folder" || props.file === "topFolder"
+      ? "white"
+      : Colors.text.secondary};
+  margin: 0;
+  margin-top: 3px;
+  z-index: 1;
+`;
