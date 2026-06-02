1a491169ee9b184c0484d0abd0466e3ded096b3c
taeyeon <binnimhi@naver.com>
2026-06-02 22:44:11 +0900

    chore::Vite 절대 경로(@) 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // path 모듈 임포트
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
