5cd14c3c5834ce20e3d8a66dccb4568c17221ae4
taeyeon <binnimhi@naver.com>
2026-06-04 19:06:49 +0900

    feat:: ProjectBox 컴포넌트 퍼블리싱

---
+++ src/components/myprojects/ProjectBox.tsx
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import { Colors } from "../../styles/color";
+
+interface ProjectBoxProps {
+  title: string;
+  text: string;
+  createdAt: string;
+  lastModifiedAt: string;
+}
+
+export default function ProjectBox({
+  title,
+  text,
+  createdAt,
+  lastModifiedAt,
+}: ProjectBoxProps) {
+  return (
+    <Wrapper>
+      <TopWrapper>
+        <ProjectName>{title}</ProjectName>
+        <ProjectDetail>{text}</ProjectDetail>
+      </TopWrapper>
+
+      <Border></Border>
+
+      <DateWrapper>
+        <DateTitle style={{ marginBottom: "6px" }}>{createdAt}</DateTitle>
+        <ShortBorder></ShortBorder>
+        <DetailDate>2026-09-03</DetailDate>
+      </DateWrapper>
+
+      <DateWrapper>
+        <DateTitle style={{ marginBottom: "16px" }}>{lastModifiedAt}</DateTitle>
+        <ShortBorder></ShortBorder>
+        <DetailDate>2026-09-08</DetailDate>
+      </DateWrapper>
+
+      <SkillContainer>
+        <SkillItem>Frontend</SkillItem>
+        <SkillItem>Frontend</SkillItem>
+        <SkillItem>Frontend</SkillItem>
+        <Plus>+2</Plus>
+      </SkillContainer>
+    </Wrapper>
+  );
+}
+
+const SkillContainer = styled.div`
+  display: flex;
+`;
+
+const Plus = styled.div`
+  color: ${Colors.text.secondary};
+  font-family: Inter;
+  font-size: 14px;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const TopWrapper = styled.div``;
+
+const ShortBorder = styled.div`
+  border: 0.5px solid ${Colors.border.default};
+  height: 12px;
+  width: 1px;
+`;
+
+const SkillItem = styled.div`
+  background-color: ${Colors.background.overlay};
+  width: 76px;
+  height: 24px;
+  padding: 4px 12px;
+  border-radius: 50px;
+  color: white;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto;
+  margin-right: 6px;
+`;
+
+const ProjectName = styled.div`
+  color: white;
+  font-size: 18px;
+  width: 100%;
+  line-height: 28px;
+`;
+
+const ProjectDetail = styled.div`
+  color: ${Colors.text.secondary};
+  font-size: 14px;
+  width: 100%;
+  line-height: 20px;
+`;
+
+const Border = styled.div`
+  border: 0.5px solid ${Colors.border.default};
+  width: 296px;
+  margin-top: 12px;
+  margin-bottom: 16px;
+  height: 1px;
+`;
+
+const DateWrapper = styled.div`
+  display: flex;
+  gap: 6px;
+`;
+
+const DateTitle = styled.div`
+  color: ${Colors.text.disabled};
+  font-size: 12px;
+`;
+
+const DetailDate = styled.div`
+  color: ${Colors.text.disabled};
+  font-size: 12px;
+`;
+
+const Wrapper = styled.div`
+  width: 360px;
+  height: 187px;
+  border: 1px solid ${Colors.background.overlay};
+  background-color: ${Colors.background.base};
+  padding: 16px 32px;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+`;
