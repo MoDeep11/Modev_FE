ac3874702f44d8885498af5998934edd7621a831
pbc1001 <mintcho0328@gmail.com>
2026-06-08 19:24:41 +0900

    fix: 불필요한 주소 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Main_2 from "../src/pages/main_page/MainPage_2";
 import Main from "../src/pages/main_page/MainPage";
 import Main_3 from "../src/pages/main_page/MainPage_3"
 import Main_4 from "../src/pages/main_page/MainPage_4"
-import End from "../src/pages/BuildProgress"
 import { Routes, Route } from "react-router-dom";
 
 
@@ -15,7 +14,6 @@ function App() {
       <Route path="/Main_3" element={<Main_2 />} />
       <Route path="/Main_2" element={<Main />} />
       <Route path="/Main_4" element={<Main_4 />}/>
-      <Route path="/End" element={<End />}/>
     </Routes> 
   );
 }
