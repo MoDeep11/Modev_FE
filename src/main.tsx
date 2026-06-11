685aaaca7951f6d9a59a6c641f60600586ca569e
taeyeon <binnimhi@naver.com>
2026-06-11 19:27:58 +0900

    feat:: browserRouter 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    ,
+  </BrowserRouter>,
 );
