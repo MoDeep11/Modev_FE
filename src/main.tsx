79f986131d0569c0f90de87dd94d3370bdf91dd3
taeyeon <binnimhi@naver.com>
2026-06-11 21:36:31 +0900

    fix:: 필요없는 줄 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
