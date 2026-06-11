d5a6d34500020ca1e4add6bf1d07f64f5c80fec5
taeyeon <binnimhi@naver.com>
2026-06-11 12:25:33 +0900

    fix:: api 호출 함수 이름 첫번째 글자 소문자로 변경

--- src/apis/auth/index.tsx
+++ src/apis/auth/index.tsx
@@ -6,7 +6,7 @@ import type {
   LoginProps,
 } from "./type";
 
-export const CreateUser = async ({
+export const createUser = async ({
   email,
   password,
   passwordConfirm,
@@ -19,29 +19,30 @@ export const CreateUser = async ({
   return response.data;
 };
 
-export const EmailCheck = async ({ token }: EmailCheckProps) => {
+export const emailCheck = async ({ token }: EmailCheckProps) => {
   const response = await api.post(`/auth/email/verify`, {
     token,
   });
   return response.data;
 };
 
-export const EmailSend = async ({ email }: EmailSendProps) => {
+export const emailSend = async ({ email }: EmailSendProps) => {
   const response = await api.post(`/auth/email/resend`, {
     email,
   });
   return response.data;
 };
 
-export const LoginUser = async ({ email, password }: LoginProps) => {
+export const loginUser = async ({ email, password }: LoginProps) => {
   const response = await api.post(`/auth/login`, {
     email,
     password,
   });
+
   return response.data;
 };
 
-export const LogoutUser = async () => {
-  const response = await api.post(`/auth/logout`, {});
+export const logoutUser = async () => {
+  const response = await api.post(`/auth/logout`);
   return response.data;
 };
