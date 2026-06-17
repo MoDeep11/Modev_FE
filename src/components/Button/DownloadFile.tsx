ed90b7038ec782564f1442cec734d245fa99eb4e
taeyeon <binnimhi@naver.com>
2026-06-17 10:38:06 +0900

    refactor:: 파일 다운로드 버튼 컴포넌트로 분리

---
+++ src/components/Button/DownloadFile.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { Colors } from "../../styles/color";
+import { useDownloadZip } from "../../hooks/downloadZip";
+import { useParams } from "react-router-dom";
+
+export default function DownloadFile() {
+  const { mutate: download, isPending } = useDownloadZip();
+  const { id } = useParams<{ id: string }>();
+
+  const downloadFunc = () => {
+    download({ projectId: Number(id) });
+  };
+
+  return isPending ? (
+    <Wrapper onClick={downloadFunc}>로딩 중...</Wrapper>
+  ) : (
+    <Wrapper onClick={downloadFunc}>.zip 파일 다운로드</Wrapper>
+  );
+}
+
+const Wrapper = styled.button`
+  padding: 10px 32px;
+  border: 1px solid ${Colors.border.strong};
+  color: White;
+  background-color: ${Colors.background.base};
+  border-radius: 10px;
+  width: 181px;
+  height: 39px;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
