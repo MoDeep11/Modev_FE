55b5b4baa27753270896a3670474ccb8b6b7c7e9
taeyeon <binnimhi@naver.com>
2026-06-17 22:23:50 +0900

    feat:: useDownloadZip 훅 추가

--- src/hooks/downloadZip.tsx
+++ src/hooks/downloadZip.tsx
@@ -1,10 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
-import { newDownloadZip } from "../apis/zip";
+import { newDownloadZip, DownloadZip } from "../apis/zip";
 import { toast } from "react-toastify";
 
 export const useNewDownloadZip = () => {
   return useMutation({
     mutationFn: newDownloadZip,
+
+    onError: () => {
+      toast.error("다운로드에 실패했습니다.");
+    },
+  });
+};
+
+export const useDownloadZip = () => {
+  return useMutation({
+    mutationFn: DownloadZip,
 
     onError: () => {
       toast.error("다운로드에 실패했습니다.");
